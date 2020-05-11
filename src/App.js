577454c27975dfd6affb6f1606954047509543d0
lyna <vuthilyna304@gmail.com>
2020-05-12 06:23:29 +0700

    push navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <div
+          className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+          <h5 className="my-0 mr-md-auto font-weight-normal">Company name</h5>
+          <nav className="my-2 my-md-0 mr-md-3 nav-item ">
+            <Navbar title='home' icon='fa fa-home'></Navbar>
+            <Navbar title='deal' icon='fa fa-gift'></Navbar>
+            <Navbar title='upload' icon='fa fa-upload'></Navbar>
+            <Navbar title='work' icon='fa fa-coffee'></Navbar>
+            <Navbar title='settings' icon='fa fa-cog'></Navbar>
+          </nav>
+          <a className="btn btn-outline-primary" href="#">Sign up</a>
+        </div>
       </header>
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <>
+             <a className="p-2 text-dark work " href="#"><i  className={this.props.icon}></i>{this.props.title}</a>
+             
+      </>
+    );
+  }
+}
+
+export default Navbar;
