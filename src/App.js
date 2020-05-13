3e59c244adb387a45710e64391d3d97c8354566a
lyna <vuthilyna304@gmail.com>
2020-05-13 15:20:53 +0700

    remix 7-2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+  render() {
+    var navbar =[
+      {
+        id:1,
+        name:'Home',
+        icon: 'fa fa-home'
+      },
+      {
+        id:2,
+        name:'deal',
+        icon: 'fa fa-gift'
+      },
+      {
+        id:3,
+        name:'upload',
+        icon: 'fa fa-upload'
+      },
+      {
+        id:4,
+        name:'work',
+        icon: 'fa fa-coffee'
+      },
+      {
+        id:5,
+        name:'setting',
+        icon: 'fa fa-cog'
+      },
+
+    ];
+    let elements= navbar.map((item,index)=>{
+      return <Navbar
+               key={item.id}
+               name={item.name}
+               icon={item.icon}
+             >
+             </Navbar>
+    });
+    return (
+      <div className="App">
+       <header className="App-header">
        
         <div
           className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
           <h5 className="my-0 mr-md-auto font-weight-normal">Company name</h5>
           <nav className="my-2 my-md-0 mr-md-3 nav-item ">
-            <Navbar title='home' icon='fa fa-home'></Navbar>
-            <Navbar title='deal' icon='fa fa-gift'></Navbar>
-            <Navbar title='upload' icon='fa fa-upload'></Navbar>
-            <Navbar title='work' icon='fa fa-coffee'></Navbar>
-            <Navbar title='settings' icon='fa fa-cog'></Navbar>
+            {elements}
           </nav>
-          <a className="btn btn-outline-primary" href="#">Sign up</a>
         </div>
-      </header>
-    </div>
-  );
+       </header>
+    </div>      
+    );
+  }
 }
 
 export default App;
+
+
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 class Navbar extends Component {
   render() {
+   
     return (
       <>
-             <a className="p-2 text-dark work " href="#"><i  className={this.props.icon}></i>{this.props.title}</a>
+             <a className="p-2 text-dark work " href="#"><i  className={this.props.icon}></i>{this.props.name}</a>
              
       </>
     );
